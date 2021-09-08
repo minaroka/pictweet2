96186cb85584a82f588749bf2eac5c05d9292ef6
minaroka <sheep.mb0401@gmail.com>
2021-09-08 19:43:21 +0900

    tweetsコントローラーのミス訂正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,6 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require (:tweet).parmit(:name, :image, :text)
+    params.require(:tweet).permit(:name, :image, :text)
   end
 end
